83a1d32d89768f9fac51b2f5c7f1b57fc771ab8e
crazyman4865 <ilian_ge@abv.bg>
2014-02-06 23:35:25 +0200

    Created project & added basic code

---
+++ crazybot 2/main.cpp
@@ -0,0 +1,11 @@
+// Don't make me explain all the basic libraries.
+#include <iostream>
+
+
+// You should really know what this function is for if you plan to
+// use/contribute to the code.
+int main()
+{
+	std::cout << "Hello eyewire!" << std::endl; // You gotta start somewhere right?
+	return 0;
+}
